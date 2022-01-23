b3969e8ebeff66cd8e96baaadafd5df4d752f2e5
unknown <subhash.k9893@gmail.com>
2022-01-23 14:47:22 +0530

    Updates

---
+++ precache-manifest.147fa1600e92b94eeabe7f058687f89c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "991e58483b2bfa64a25172bc5a6d8f4a",
+    "url": "/reactapp/index.html"
+  },
+  {
+    "revision": "537aecc712d3245df841",
+    "url": "/reactapp/static/css/2.68a81a89.chunk.css"
+  },
+  {
+    "revision": "77e45e6254ff5942edda",
+    "url": "/reactapp/static/css/main.a110a6ab.chunk.css"
+  },
+  {
+    "revision": "537aecc712d3245df841",
+    "url": "/reactapp/static/js/2.81d30f09.chunk.js"
+  },
+  {
+    "revision": "cafb2900a48672235c18",
+    "url": "/reactapp/static/js/3.428fee09.chunk.js"
+  },
+  {
+    "revision": "77e45e6254ff5942edda",
+    "url": "/reactapp/static/js/main.42bc2e29.chunk.js"
+  },
+  {
+    "revision": "4cd63cf9697ca6e23a5d",
+    "url": "/reactapp/static/js/runtime~main.da1f17f5.js"
+  }
+]);
